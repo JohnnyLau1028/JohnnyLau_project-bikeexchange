31e4ad52b61a40789a40e3d49ed8a24543bbae56
JohnnyLau1028 <81736997+JohnnyLau1028@users.noreply.github.com>
2021-06-08 17:16:44 -0400

    nav

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,17 @@
-console.log("")
+import $ from "jquery";
+
+let $nav = $("nav[data-nav]");
+let isVisible = false;
+
+console.log("working");
+
+$(".nav-btns").on("click", function () {
+	console.log("click");
+	if (isVisible === false) {
+		$nav.show();
+		isVisible = true;
+	} else {
+		$nav.hide();
+		isVisible = false;
+	}
+});
